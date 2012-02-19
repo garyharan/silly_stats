9e711ca6d30305ebaa8247d9655b706b0a3c8471
Gary Haran <gary@openplaces.com>
2012-02-19 15:41:10 -0500

    added all files needed for example

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,5 @@
 class StatsController < ActionController::Base
   def index
-    @tables = ActiveRecord::Base.connection.tables
+    @tables = ActiveRecord::Base.connection.tables.reject {|t| t == 'schema_migrations' }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get "stats/index"
+
   match 'stats', :to => 'stats#index'
 end
