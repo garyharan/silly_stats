95dc9e0f5ed5253546c4312ea1e8a8f30861e833
Gary Haran <gary@openplaces.com>
2012-02-20 08:36:59 -0500

    updated readme and cleaned up things a bit

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,6 @@
 class StatsController < ActionController::Base
   def index
-    @tables = ActiveRecord::Base.connection.tables.reject {|t| t == 'schema_migrations' }
+    tables  = ActiveRecord::Base.connection.tables.reject { |t| t == 'schema_migrations' }
+    @models = tables.map { |t| Kernel.const_get t.classify }
   end
 end
